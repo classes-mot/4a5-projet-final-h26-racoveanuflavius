9255f3575beea83d6e51aed2930fe85d9c291862
Flavius Racoveanu <racoveanuflav@gmail.com>
2026-04-29 14:36:32 -0400

    Création projet

---
+++ backend/util/bd.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+let isConnected = false;
+
+export const connectDB = async () => {
+  if (isConnected) return;
+
+  let uri = 'mongodb://localhost:27017/UrbexMania';
+
+  try {
+    await mongoose.connect(uri);
+    isConnected = true;
+    console.log('Connexion au BD réussie');
+  } catch (err) {
+    console.log('Connexion au BD échouéeco', err);
+    process.exit(1);
+  }
+};
