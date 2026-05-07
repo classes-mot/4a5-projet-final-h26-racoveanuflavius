12000b8b1127ba860396d67e088c1fecb360abf0
Flavius Racoveanu <racoveanuflav@gmail.com>
2026-05-06 20:17:41 -0400

    ajout des constants photos et dangers

---
+++ frontend/src/constants/buildingOptions.js
@@ -0,0 +1,23 @@
+//Afin de ne pas me répéter 10000000 fois, j'ai créer ce fichier pour réutiliser certaines constantes
+
+export const dangerOptions = [
+  "Amiante",
+  "Structure instable",
+  "Présence de squatters",
+  "Inondation",
+  "Contamination chimique",
+  "Électricité défectueuse",
+  "Aucun",
+];
+
+export const imageOptions = [
+  { label: "Hôpital abandonné", value: "hopital.png" },
+  { label: "Usine abandonnée", value: "usine.png" },
+  { label: "École abandonnée", value: "ecole.png" },
+  { label: "Église abandonnée", value: "eglise.png" },
+  { label: "Manoir abandonné", value: "manoir.png" },
+  { label: "Hôtel abandonné", value: "hotel.png" },
+  { label: "Prison abandonnée", value: "prison.png" },
+  { label: "Centre commercial abandonné", value: "centreCommercial.png" },
+  { label: "Autre", value: "autre.png" },
+];
