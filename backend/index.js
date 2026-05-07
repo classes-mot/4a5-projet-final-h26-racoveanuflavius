7e1cb09202bb28b2211a7da4e16dbaa6bdedd8e4
Flavius Racoveanu <racoveanuflav@gmail.com>
2026-05-06 20:14:13 -0400

    debut frontend, images

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import batimentsRoutes from "./routes/batiments-routes.js";
+import authRoutes from "./routes/auth-routes.js";
+import usersRoutes from "./routes/users-routes.js";
 import cors from 'cors';
 import errorHandler from './handler/error-Handler.js';
 import { connectDB } from './util/bd.js';
@@ -8,15 +11,15 @@ app.use(express.json());
 app.use(cors())
 
 
+app.use("/auth", authRoutes);
+app.use("/users", usersRoutes);
+app.use("/buildings", batimentsRoutes);
+
 app.use((req, res, next) => {
   const error = new Error('Route non trouvée');
   error.code = 404;
   next(error);
 });
-
-app.use('/api/users', usersRoutes)
-app.use('/api/batiments', batimentsRoutes)
-
 
 app.use(errorHandler);
 
